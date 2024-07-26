78231e09495522ddfb1c2bbf50cf28b7a2e252cf
Janishar Ali <janishar.ali@gmail.com>
2024-07-26 17:41:55 +0530

    add graphalgo

--- src/dslib/graph.h
+++ src/dslib/graph.h
@@ -58,8 +58,11 @@ extern "C"
 	void graph_print(Graph *graph, DataToString tostring);
 	void graph_destroy(Graph *graph);
 
-	// utils
+	// utility functions : graphutil.c
 	Graph *graph_from_2d_arr(char **arr, int rows, int cols, bool autofree);
+
+	// algorithms functions : graphalgo.c
+	
 
 #ifdef __cplusplus
 }

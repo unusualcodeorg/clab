ace6e7e9cfefae3d2caf1f60ea64a2a13e2baa9f
Janishar Ali <janishar.ali@gmail.com>
2024-07-29 13:42:04 +0530

    restructure code

--- src/puzzle/maze.h
+++ src/puzzle/maze.h
@@ -1,7 +1,4 @@
 
-#include "../dslib/stack.h"
-#include "../dslib/util.h"
-
 #ifndef CLAB_PUZZLE_MAZE_H
 #define CLAB_PUZZLE_MAZE_H
 
@@ -10,8 +7,6 @@ extern "C" {
 #endif
 
 int maze_shortest_distance(void);
-Graph2DMap *maze_graph_map_create(char ***arr, int rows, int cols, bool autofree);
-void maze_sd_result_print(Stack *stack, char ***arr, int cols);
 
 #ifdef __cplusplus
 }
